ca89bf1b822f080f8e03ba50d00b40dda4381a13
GreenQ <tenny47@gmail.com>
2015-05-07 14:12:57 +0300

    Code logic

--- app/src/main/java/com/dailyappslab/tfms/MarketActivity.java
+++ app/src/main/java/com/dailyappslab/tfms/MarketActivity.java
@@ -3,19 +3,36 @@ package com.dailyappslab.tfms;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by GreenQ on 06.05.2015.
  */
 public class MarketActivity extends Activity {
+    Button btnClose;
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameshop);
+        btnClose = (Button) findViewById(R.id.btnClose);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-    public void CloseDialog(View view)
-    {
-        finish();
-    }
+//    public void CloseDialog(View view)
+//    {
+//        try {
+//           onBackPressed();
+//        }
+//        catch (Exception ex)
+//        {
+//            Globals.DisplayAlert(this, ex.getMessage(), "Произошла непредвиденная ошибка");
+//        }
+//    }
 }
