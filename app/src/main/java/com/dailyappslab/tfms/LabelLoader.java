443b239568cb5d07b51d06c092ae5fd6a4ef34cf
AsCross <tarasdudyak@gmail.com>
2015-05-07 20:36:29 +0300

    баловство з комітами =)

--- app/src/main/java/com/dailyappslab/tfms/LabelLoader.java
+++ app/src/main/java/com/dailyappslab/tfms/LabelLoader.java
@@ -1,15 +1,20 @@
 package com.dailyappslab.tfms;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
 
 /**
  * Created by GreenQ on 05.05.2015.
  */
 public class LabelLoader extends ActionBarActivity {
+
+    private AnimationDrawable mAnimationDrawable;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,13 @@ public class LabelLoader extends ActionBarActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        ImageView imageView = (ImageView) findViewById(R.id.loadlabel);
+        imageView.setBackgroundResource(R.drawable.loadanim);
+
+        mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
+
+        mAnimationDrawable.start();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -29,6 +41,6 @@ public class LabelLoader extends ActionBarActivity {
                 //overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
                 finish();
             }
-        }, 5000);
+        }, 1500);
     }
 }
